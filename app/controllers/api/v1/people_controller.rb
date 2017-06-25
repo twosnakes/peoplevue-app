8d0f73a4677d93d3b317637359f67fe894f8a568
Zoe Lembeck <zlembeck@gmail.com>
2017-06-25 11:23:42 -0500

    added sort features via bio ascendign and descending and letter

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -5,7 +5,10 @@ class Api::V1::PeopleController < ApplicationController
 
   def create
     @person = Person.new(name: params[:name], bio:params[:bio])
-    @person.save
-    render 'show.json.jbuilder'
+    if @person.save
+      render 'show.json.jbuilder'
+    else 
+      render json: { errors: @person.errors.full_messages }, status: 422
+    end
   end
 end
